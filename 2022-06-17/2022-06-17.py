2a0cc203d5661e9c58b27d0f3ccb44fa5d1c8acc
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-17 18:52:52 +0200

    Update 2022-06-17.py

--- 2022-06-17/2022-06-17.py
+++ 2022-06-17/2022-06-17.py
@@ -1,1 +1,17 @@
-"place holder comit via phone"
+import pandas as pd
+
+"""
+cleaning data duplicates get dropped of you can replace them with a loop 
+
+
+"""
+
+def main():
+    df = pd.read_csv("data.csv")
+
+    print(df.duplicated())
+    df.drop_duplicates(inplace = True)
+
+
+if __name__ == '__main__':
+    main()
