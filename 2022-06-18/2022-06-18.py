fcb4f86a977cd79de786750fca8022ab33b85edf
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-18 13:56:02 +0200

    Update 2022-06-18.py

--- 2022-06-18/2022-06-18.py
+++ 2022-06-18/2022-06-18.py
@@ -1,3 +1,40 @@
+import pandas as pd
+
 """
-place holder - commits via phone
+Series
+    series is just a column 
+    you can label your column with new ones (index = [...])
+     
+
 """
+
+def main():
+    arr = [0, 4, 12, 1, 8]
+    print(pd.Series(arr))
+
+    print(pd.Series(arr)[0])
+
+    newArr = pd.Series(arr, index = ["a", "b", "c", "d", "e"])
+    print(newArr["d"])
+
+def keys():
+    setArr = {
+        "28-01": 1531,
+        "29-01": 451,
+        "30-01": 2313,
+    }
+
+    mySet = pd.Series(setArr, index=["29-01", "28-01"])
+    print(mySet)
+
+def dataFrameFunc():
+    setArr = {
+        "28-01": [1531],
+        "29-01": [451],
+        "30-01": [2313],
+    }
+
+    print(pd.DataFrame(setArr))
+
+if __name__ == '__main__':
+    dataFrameFunc()
