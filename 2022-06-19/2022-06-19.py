ce70e29056b8ce3ef9693f0a3615c21234178eac
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-19 14:06:26 +0200

    Update 2022-06-19.py

--- 2022-06-19/2022-06-19.py
+++ 2022-06-19/2022-06-19.py
@@ -1,3 +1,21 @@
-"""
-place holder - commits via phone
-"""
+
+import pandas as pd
+
+def main():
+    data = {
+        "items": [4, 231, 20],
+        "water": [0, 231, 1312],
+
+    }
+
+    df = pd.DataFrame(data)
+    print(df)
+    print(df.loc[0])
+
+    ndf = pd.DataFrame(data, index=["Ger", "Aus", "It"])
+    print(ndf)
+
+    print(ndf.loc["Ger"])
+
+if __name__ == '__main__':
+    main()
