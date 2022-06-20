d37315672f71b4c4a52867a6163508a0235639d8
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-20 07:29:37 +0200

    Update 2022-06-20.py

--- 2022-06-20/2022-06-20.py
+++ 2022-06-20/2022-06-20.py
@@ -1,3 +1,21 @@
+inport pandas as pd 
+
+reading csv files
+
+
 """
-place holder - commits via phone
-"""
+
+def main():
+    df = pd.read_csv('data.csv')
+
+    print(df.to_string())
+
+def system():
+    print(pd.options.display.max_rows)
+
+    """ change max rows"""
+    pd.options.display.max_rows = 100
+
+
+if __name__ == '__main__':
+    system()
