6469791972df524935d793040d48c07c2b6d2948
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-21 08:49:12 +0200

    Update 2022-06-21.py

--- 2022-06-21/2022-06-21.py
+++ 2022-06-21/2022-06-21.py
@@ -1,3 +1,22 @@
+
+
+import pandas as pd
+
 """
-place holder - commits via phone
+reading json files 
+
+Json files = a python dictionary
+
+usage is the same ==
+
+
 """
+
+def main():
+    df = pd.read_json('data.json')
+    print(df.to_string())
+
+
+
+if __name__ == '__main__':
+    main()
