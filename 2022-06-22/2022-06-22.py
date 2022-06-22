bcdaee9de76acb8f198322df6dfc4c7cc29bf6f2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-22 08:46:35 +0200

    Update 2022-06-22.py

--- 2022-06-22/2022-06-22.py
+++ 2022-06-22/2022-06-22.py
@@ -1,3 +1,22 @@
+import pandas as pd
+
 """
-place holder - commits via phone
+df.head(n) n = count
+
+df.head() == first 5 rows
+df.tail() == last 5 rows 
+.info() == returns info 
+
 """
+
+def main():
+    df = pd.read_csv('data.csv')
+
+    print(df.head(10))
+    print(df.tail())
+    print(df.info())
+
+
+
+if __name__ == '__main__':
+    main()
