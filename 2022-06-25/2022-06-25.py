aba8d48fc47f8ce510215fcf57b96c01e3877aac
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-25 14:40:53 +0200

    Update 2022-06-25.py

--- 2022-06-25/2022-06-25.py
+++ 2022-06-25/2022-06-25.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
 """
-place holder - commits via phone
+Wrong format data 
+
+pd.to_datetime(df['Date'])
+converts 
+20220412
+to 2022/04/12
+
+nan to nat 
+
+
+df.dropna(subset=['Date'], inplace = True)
+drops the whole row
+
 """
+
+
+def main():
+
+    df = pd.read_csv('data.csv')
+
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    df.dropna(subset=['Date'], inplace = True)
+
+
+if __name__ == '__main__':
+    main()
