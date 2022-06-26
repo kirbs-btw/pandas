5807a38b63c7fe1dd88cb55a269f2c01d0a31913
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-06-26 15:29:10 +0200

    Add files via upload

--- 2022-06-26/2022-06-26.py
+++ 2022-06-26/2022-06-26.py
@@ -1,3 +1,27 @@
+import pandas as pd
+
 """
-place holder - commits via phone
+df.loc()
+change loc()
+
+
 """
+
+
+def main():
+
+    df = pd.read_csv("data.csv")
+
+    df.lov[7, 'Duration'] = 45
+
+    for x in df.index:
+        if df.loc[x, "Duration"] > 120:
+            df.loc[x, "Duration"] = 120
+
+    for x in df.index:
+        if df.loc[x, "Duration"] > 120:
+            df.drop(x, inplace=True)
+
+
+if __name__ == '__main__':
+    main()
